d4e3d17c44a114ca00e16324e728ba0b8cb9ed83
asapaska <asapaska3@gmail.com>
2026-03-30 17:37:00 +0300

    wranger update

--- src/worker.js
+++ src/worker.js
@@ -2,11 +2,12 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    return new Response(
-      `Worker is running.\nPath: ${url.pathname}`,
-      {
+    if (url.pathname === "/discord/interactions") {
+      return new Response("Discord endpoint is ready.", {
         headers: { "Content-Type": "text/plain; charset=utf-8" }
-      }
-    );
+      });
+    }
+
+    return env.ASSETS.fetch(request);
   }
 };
