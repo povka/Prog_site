fd5c86c024c3c7af0798343033173305af2f4386
asapaska <asapaska3@gmail.com>
2026-03-30 17:25:02 +0300

    discord endpoint

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,13 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Thicc Magician Girl is alive.", {
-      headers: { "Content-Type": "text/plain" }
-    });
+    const url = new URL(request.url);
+
+    if (url.pathname === "/discord/interactions") {
+      return new Response("Discord endpoint is ready.", {
+        headers: { "Content-Type": "text/plain" }
+      });
+    }
+
+    return env.ASSETS.fetch(request);
   }
 };
